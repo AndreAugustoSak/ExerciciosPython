e9404ebe3ae1b6250135343c3f3150a21c2469e2
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-13 22:27:20 -0300

    Create EstruturasDeDecisao_05.py

---
+++ EstruturasDeDecisao_05.py
@@ -0,0 +1,17 @@
+# Faça um programa para a leitura de duas notas parciais de um aluno.
+# O programa deve calcular a média alcançada por aluno e apresentar:
+# A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+# A mensagem "Reprovado", se a média for menor do que sete;
+# A mensagem "Aprovado com Distinção", se a média for igual a dez.
+
+def nota():
+    n1 = float(input('Digite a primeira nota: '))
+    n2 = float(input('Digite a segunda nota: '))
+    m = ((n1+n2)/2)
+    if m == 10:
+        print('Sua média foi {} e você foi Aprovado com Distinção.'.format(m))
+    elif 7 <= m <= 9.9:
+        print('Sua média foi {} e você foi Aprovado.'.format(m))
+    else:
+        print('Sua média foi {} e você foi Reprovado.'.format(m))
+nota()
