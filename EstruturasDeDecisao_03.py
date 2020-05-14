05582de327a80129525b6a905f732883e2b8a78c
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-13 21:30:58 -0300

    Create EstruturasDeDecisao_03.py

---
+++ EstruturasDeDecisao_03.py
@@ -0,0 +1,10 @@
+#Faça um Programa que verifique se uma letra digitada é "F" ou "M". Conforme a letra escrever: F - Feminino, M - Masculino, Sexo Inválido.
+def sexo(n):
+    n = str(input('Digite M para masculino e F para feminino: ')).upper().strip()
+    if n == 'F':
+        print('F - Feminino')
+    elif n == 'M':
+        print('M - Masculino')
+    else:
+        print('Sexo Inválido')
+sexo(0)
