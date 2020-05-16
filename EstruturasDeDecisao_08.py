fdb8e394ef2996b58bde102f6b58b531a6f48c84
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-15 23:37:31 -0300

    Create EstruturasDeDecisao_08.py

---
+++ EstruturasDeDecisao_08.py
@@ -0,0 +1,21 @@
+# Faça um programa que pergunte o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
+def preco ():
+    import numpy as np
+    n1 = float(input('Qual o preço do primeiro produto? R$ '))
+    n2 = float(input('Qual o preço do segundo produto? R$ '))
+    n3 = float(input('Qual o preço do terceiro produto? R$ '))
+    if n1 == n2 == n3:
+        print('Os produtos todos têm o mesmo preço.')
+    if n1 == n2 and n1 < n3:
+        print('O primeiro e o segundo produtos são os mais baratos.')
+    if n1 == n3 and n1 < n2:
+        print('O primeiro e o terceiro produtos são os mais baratos.')
+    if n2 == n3 and n1 > n2:
+        print('O segundo e o terceiro produtos são os mais baratos.')
+    if n1 < n2 and n1 < n3:
+        print('O primeiro produto é o mais barato.')
+    if n2 < n1 and n2 < n3:
+        print('O segundo produto é o mais barato.')
+    if n3 < n1 and n3 < n2:
+        print('O terceiro protudo é o mais barato.')
+preco()
