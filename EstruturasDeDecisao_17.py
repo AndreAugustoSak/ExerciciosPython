eca6f6ef77ad68b5d0883f354aa1f903af3288d7
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-17 21:16:36 -0300

    Create EstruturasDeDecisao_17.py

---
+++ EstruturasDeDecisao_17.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não bissexto.
+def bissexto():
+    ano = int(input('Que ano quer analisar? '))
+    if ano % 4 == 0 and not ano % 100 == 0 or ano % 400 == 0:
+        print('O ano {} é BISSEXTO.'.format(ano))
+    else:
+        print('O ano {} NÃO é BISSEXTO.'.format(ano))
+bissexto()
