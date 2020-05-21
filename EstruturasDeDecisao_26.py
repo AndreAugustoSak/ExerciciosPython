90752c89f69657988f5ce6a419279f896e01a61b
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-21 14:36:38 -0300

    Update EstruturasDeDecisao_26.py

--- EstruturasDeDecisao_26.py
+++ EstruturasDeDecisao_26.py
@@ -6,7 +6,7 @@
   # até 20 litros, desconto de 4% por litro
  # acima de 20 litros, desconto de 6% por litro.
 # Escreva um algoritmo que leia o número de litros vendidos, o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 2,50 o preço do litro do álcool é R$ 1,90.
-def combustivel():
+def estrutura_decisao_26():
     tipo = str(input('''Qual tipo de combustível você quer? 
 Digite: 
 A - para Álcool
@@ -27,4 +27,4 @@ G - para Gasolina
         valor = litros * 2.5 * 0.94
         tipo = 'gasolina'
     print('Para abastecer {} litros de {}, você deverá pagar R$ {:.2f}.'.format(litros, tipo, valor))
-combustivel()
+estrutura_decisao_26()
