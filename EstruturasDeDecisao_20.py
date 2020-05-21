04c006a5d4bd3e58124cad8f54de974bf3577c19
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-21 14:36:18 -0300

    Update EstruturasDeDecisao_20.py

--- EstruturasDeDecisao_20.py
+++ EstruturasDeDecisao_20.py
@@ -2,7 +2,7 @@
  # A mensagem "Aprovado", se a média for maior ou igual a 7, com a respectiva média alcançada;
  # A mensagem "Reprovado", se a média for menor do que 7, com a respectiva média alcançada;
  # A mensagem "Aprovado com Distinção", se a média for igual a 10.
-def resultado():
+def estrutura_decisao_20():
     n1 = float(input('Digite a primeira nota: '))
     n2 = float(input('Digite a segunta nota: '))
     n3 = float(input('Digite a terceira nota: '))
@@ -13,4 +13,4 @@ def resultado():
         print('Sua média foi {:.2f} e você foi APROVADO.'.format(m))
     else:
         print('Sua média foi {:.2f} e você foi REPROVADO.'.format(m))
-resultado()
+estrutura_decisao_20()
