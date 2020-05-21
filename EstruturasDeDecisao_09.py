9931172d5a79a22d602ab781b277532bab1014c7
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-21 14:35:43 -0300

    Update EstruturasDeDecisao_09.py

--- EstruturasDeDecisao_09.py
+++ EstruturasDeDecisao_09.py
@@ -1,9 +1,9 @@
 # Faça um Programa que leia três números e mostre-os em ordem decrescente.
-def decrescente ():
+def estrutura_decisao_09():
     n1 = float(input('Digite o primeiro número: '))
     n2 = float(input('Digite o segundo número: '))
     n3 = float(input('Digite o terceiro número: '))
     list = [n1, n2, n3]
     ordem = sorted(list, reverse=True)
     print('A ordem decrescente dos número é :', ordem)
-decrescente()
+estrutura_decisao_09()
