87d92caa99c74b4a56b7d0fd6bb12e6a855b6550
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-21 14:35:30 -0300

    Update EstruturasDeDecisao_05.py

--- EstruturasDeDecisao_05.py
+++ EstruturasDeDecisao_05.py
@@ -4,7 +4,7 @@
 # A mensagem "Reprovado", se a média for menor do que sete;
 # A mensagem "Aprovado com Distinção", se a média for igual a dez.
 
-def nota():
+def estrutura_decisao_05():
     n1 = float(input('Digite a primeira nota: '))
     n2 = float(input('Digite a segunda nota: '))
     m = ((n1+n2)/2)
@@ -14,4 +14,4 @@ def nota():
         print('Sua média foi {} e você foi Aprovado.'.format(m))
     else:
         print('Sua média foi {} e você foi Reprovado.'.format(m))
-nota()
+estrutura_decisao_05()
