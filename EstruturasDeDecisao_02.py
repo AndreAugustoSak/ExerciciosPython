eee2d09b8fb6e50d6da962bbafb44d3e8d0c9a81
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-21 14:35:23 -0300

    Update EstruturasDeDecisao_02.py

--- EstruturasDeDecisao_02.py
+++ EstruturasDeDecisao_02.py
@@ -1,8 +1,8 @@
 # Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
-def valor():
+def estrutura_decisao_02():
     n = float(input('Digite um número: '))
     if n < 0:
         print('O número {} é negativo.'.format(n))
     else:
         print('O número {} é positivo.'.format(n))
-valor()
+estrutura_decisao_02()
