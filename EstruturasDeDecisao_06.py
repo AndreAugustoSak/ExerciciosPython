c0a84e40d2f648f53b37a222426041f678bb76f5
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-21 14:35:33 -0300

    Update EstruturasDeDecisao_06.py

--- EstruturasDeDecisao_06.py
+++ EstruturasDeDecisao_06.py
@@ -1,8 +1,8 @@
 # Faça um Programa que leia três números e mostre o maior deles.
-def numero():
+def estrutura_decisao_06():
     n1 = float(input('Digite um número: '))
     n2 = float(input('Digite um outro número: '))
     n3 = float(input('Digite o último número: '))
     lista = [n1, n2, n3]
     print('O maior número é {}.'.format(max(lista)))
-numero()
+estrutura_decisao_06()
