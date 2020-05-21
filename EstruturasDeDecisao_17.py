94e1d8ea294b7ba43e0acd60340cb0a416928af9
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-21 14:50:54 -0300

    Update EstruturasDeDecisao_17.py

--- EstruturasDeDecisao_17.py
+++ EstruturasDeDecisao_17.py
@@ -1,8 +1,8 @@
 # Faça um Programa que peça um número correspondente a um determinado ano e em seguida informe se este ano é ou não bissexto.
-def strutura_decisao_17():
+def estrutura_decisao_17():
     ano = int(input('Que ano quer analisar? '))
     if ano % 4 == 0 and not ano % 100 == 0 or ano % 400 == 0:
         print('O ano {} é BISSEXTO.'.format(ano))
     else:
         print('O ano {} NÃO é BISSEXTO.'.format(ano))
-strutura_decisao_17()
+estrutura_decisao_17()
