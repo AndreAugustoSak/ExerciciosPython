ad04591dfca8450de04a354bc7c1bef80b669e93
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-21 14:36:25 -0300

    Update EstruturasDeDecisao_22.py

--- EstruturasDeDecisao_22.py
+++ EstruturasDeDecisao_22.py
@@ -1,8 +1,8 @@
 # Faça um Programa que peça um número inteiro e determine se ele é par ou impar. Dica: utilize o operador módulo (resto da divisão).
-def numero():
+def estrutura_decisao_22():
     n = int(input('Informe um número inteiro: '))
     if n % 2 == 0:
         print('{} é um número par.'.format(n))
     else:
         print('{} é um número ímpar.'.format(n))
-numero()
+estrutura_decisao_22()
