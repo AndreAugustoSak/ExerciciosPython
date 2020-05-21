4a24cee95969081d3cd86368cfc30e129746975c
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-20 21:51:08 -0300

    Update EstruturasDeDecisao_19.py

--- EstruturasDeDecisao_19.py
+++ EstruturasDeDecisao_19.py
@@ -1,6 +1,6 @@
 # Faça um Programa que leia um número inteiro menor que 1000 e imprima a quantidade de centenas, dezenas e unidades do mesmo.
 import __future__
-def numeros():
+def numeros(n):
     n = int(input('Digite um número inteiro menor que mil: '))
     if n >= 1000 or n < 0:
         print('Numero inválido!')
@@ -10,4 +10,4 @@ def numeros():
 else ', ' + ' '.join(x)+('' if x[0] == '1' else 's') for x in
 zip(str(n)[::-1], ('centena', 'dezena',
 'unidade')[::-1])]))[::-1])[2:][::-1].replace(' ,', ' e ', 1)[::-1])
-numeros()
+numeros(326)
