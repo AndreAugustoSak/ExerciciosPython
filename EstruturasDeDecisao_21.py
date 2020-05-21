df790dc4eaefa32310433f0fb9a997d8c8005d37
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-21 14:36:21 -0300

    Update EstruturasDeDecisao_21.py

--- EstruturasDeDecisao_21.py
+++ EstruturasDeDecisao_21.py
@@ -1,5 +1,5 @@
 # Faça um Programa para um caixa eletrônico. O programa deverá perguntar ao usuário a valor do saque e depois informar quantas notas de cada valor serão fornecidas. As notas disponíveis serão as de 1, 5, 10, 50 e 100 reais. O valor mínimo é de 10 reais e o máximo de 600 reais. O programa não deve se preocupar com a quantidade de notas existentes na máquina.
-def saque():
+def estrutura_decisao_21():
     valor = int(input('Qual o valor do saque? '))
     notas_100 = valor // 100
     notas_50 = (valor - notas_100 * 100) // 50
@@ -12,4 +12,4 @@ def saque():
 {} nota(s) de R$ 10,00;
 {} nota(s) de R$ 5,00;
 {} nota(s) de R$ 1,00.'''.format(valor, notas_100, notas_50, notas_10, notas_5, notas_1))
-saque()
+estrutura_decisao_21()
